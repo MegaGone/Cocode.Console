785340b4c7371f61dbeca0acb67faffccbf09857
MegaGone <jimmymartinez016@gmail.com>
2024-10-21 20:52:47 -0600

    [FIX] - Last deployment

--- Cocode.Console.Frontend/src/environments/environment.base.ts
+++ Cocode.Console.Frontend/src/environments/environment.base.ts
@@ -1,6 +1,6 @@
 export let environment = {
     production: true,
     hmr: false,
-    base_url: 'http://localhost:3000/api',
-    // base_url: 'https://api.cocodecuilco.com/api',
+    // base_url: 'http://localhost:3000/api',
+    base_url: 'https://api.cocodecuilco.com/api',
 };
--- Cocode.Console.Frontend/src/environments/environment.hmr.ts
+++ Cocode.Console.Frontend/src/environments/environment.hmr.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     hmr: true,
-    base_url: 'http://localhost:3000/api',
-    // base_url: 'https://api.cocodecuilco.com/api',
+    // base_url: 'http://localhost:3000/api',
+    base_url: 'https://api.cocodecuilco.com/api',
 };
